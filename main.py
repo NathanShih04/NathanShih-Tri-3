754bb7e87a669d7c38e969315ad12c33f65ac162
TankeeTort <88810038+NathanShih04@users.noreply.github.com>
2022-03-21 06:46:28 +0000

    Fix keypad

--- main.py
+++ main.py
@@ -115,35 +115,24 @@ while ans:
         
       # Matrix Numberpad
       elif ans == "4":
+        R = int(3)
+        C = int(3)
+
+        # Initialize matrix
         matrix = []
-        for i  in range (1):
+        print("Enter the entries rowwise:")
         
-          matrix.append([])
-          for j in range (1,4):
-            matrix [i].append(j)
+        # For user input
+        for i in range(R):          # A for loop for row entries
+            a =[]
+            for j in range(C):      # A for loop for column entries
+                 a.append(int(input()))
+            matrix.append(a)
         
-        matrix2 = []
-        for i in range (1):
-          matrix2.append([])
-          for k in range (4,7):
-            matrix2 [i].append(k)
-        
-        matrix3 = []
-        for i in range (1):
-          matrix3.append ([])
-          for g in range (7,10):
-            matrix3 [i].append(g)
-        
-        print(matrix)
-        
-        print(matrix2)
-        print(matrix3)
-        
-        def print_matrix1(matrix):
-          print("lol")
-          for i in range(len(matrix)): 
-            for j in range(len(matrix[i])):
-              print(matrix[i][j], end=" ")
+        # For printing the matrix
+        for i in range(R):
+            for j in range(C):
+                print(matrix[i][j], end = " ")
             print()
             
       else:
