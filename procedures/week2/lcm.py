1bf1143db90a28570fad2dc425c90df1836d44fc
TankeeTort <88810038+NathanShih04@users.noreply.github.com>
2022-04-04 21:25:45 +0000

    class

--- procedures/week2/lcm.py
+++ procedures/week2/lcm.py
@@ -9,6 +9,23 @@ def lcm(a,b):
       maximum = maximum + 1
   return maximum
 
+class LCM:
+  def __init__(self, num1, num2):
+    self.num1 = num1
+    self.num2 = num2
+
+  def __call__(self):
+    if self.num1 > self.num2:
+      max = self.num1
+      
+    else:
+      max = self.num2
+    while (True):
+      if (max % self.num1 == 0 and max % self.num2 == 0):
+        break
+      max = max + 1
+    return max
+
 def perform():
   print("The LCM of 38, 12 is", lcm(38,12))
   print("The LCM of 94, 11 is",lcm(94, 11))
